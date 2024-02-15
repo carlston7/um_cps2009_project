6add4b1a11818bcb43c658a0ec92d278e08e6fde
JacobEllul <jacob.ellul.22@um.edu.mt>
2024-02-15 15:09:05 +0100

    setting up environment

--- backend/myproject/urls.py
+++ backend/myproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for projectCode project.
+URL configuration for myproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include, re_path
+from django.views.generic import RedirectView
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    re_path(r'^$', RedirectView.as_view(url='/api/', permanent=False)),  # Redirect root to /api/
 ]
+
+
