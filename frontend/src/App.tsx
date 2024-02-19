092e1b59d0650a85e86a74ac78a2179425f5f422
JacobEllul <jacob.ellul.22@um.edu.mt>
2024-02-19 13:11:53 +0100

    working connection+azure(not setup completely)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import MyComponent from './apiCall';
 
 function App() {
-  // State to manage the visibility of MyComponent
-  const [showComponent, setShowComponent] = useState(false);
-
   return (
     <div className="App">
-      <button onClick={() => setShowComponent(true)}>Load Data</button>
-      {showComponent && <MyComponent />}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
