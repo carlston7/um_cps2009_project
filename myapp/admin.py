2898561cc32038869529f956a9c0ebf1c170c5f8
JacobEllul <jacob.ellul.22@um.edu.mt>
2024-02-22 16:54:08 +0100

    shitty savepoint

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Court, Review
+from .models import Court
 
 admin.site.register(Court)
-admin.site.register(Review)
 
--- myapp/models.py
+++ myapp/models.py
@@ -1,12 +1,36 @@
-from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# class Review(models.Model):
-#     restaurant = models.ForeignKey(Court, on_delete=models.CASCADE)
-#     user_name = models.CharField(max_length=20)
-#     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-#     review_text = models.CharField(max_length=500)
-#     review_date = models.DateTimeField('review date')
+# Creating User model
+class User(models.Model):
+    user_id = models.CharField(max_length = 20, primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField(max_length=50, unique=True)
 
-#     def __str__(self):
-#         return f"{self.restaurant.name} ({self.review_date:%x})"
+# Creating CourtType model
+class CourtType(models.Model):
+    type_id = models.AutoField(primary_key=True)
+
+    CLAY = "C"
+    GRASS = "G"
+    HARD = "H"
+    COURT_TYPES = [
+        (CLAY, "Clay"),
+        (GRASS, "Grass"),
+        (HARD, "Hard")
+    ]
+    type = models.CharField(max_length=3, choices=COURT_TYPES, default=CLAY)
+
+# Creating Court model
+class Court(models.Model):
+    court_id = models.AutoField(primary_key=True)
+    type_id = models.ForeignKey("CourtType", on_delete=models.CASCADE)
+
+# Creating Booking model
+class Booking(models.Model):
+    booking_id = models.AutoField(primary_key=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    cost = models.DecimalField(max_digits=5, decimal_places=2)
+    court_id = models.ForeignKey("Court", on_delete=models.CASCADE)
+    user_id = models.ForeignKey("User", on_delete=models.CASCADE)
