268c6239883fd1a14e8979b2ba6516cce7bd8b46
Jacob <jacob.ellul.22@um.edu.mt>
2024-02-24 15:57:18 +0100

    idek anymore 2

--- backend/urls.py
+++ backend/urls.py
@@ -15,14 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from myapp.views import frontend, test_api
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/test/', test_api, name='test_api'),
     # Route for serving frontend
     path('', frontend, name='frontend')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
