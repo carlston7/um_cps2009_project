ec9fbead63fa68b89fdc2ec515cc5037b97aaad6
Jacob <jacob.ellul.22@um.edu.mt>
2024-02-27 16:53:44 +0100

    revert

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8caaabcef5c75a302fe05942166303fd.js"
+  "/precache-manifest.644e71687c6355eb94a8304f5edc9d4c.js"
 );
 
 self.addEventListener('message', (event) => {
