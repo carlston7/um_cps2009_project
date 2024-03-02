e71a5667a2223aab62020b69fba1ae1f46cc1696
JacobEllul <jacob.ellul.22@um.edu.mt>
2024-03-02 18:02:49 +0100

    todo

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,13 +5,14 @@ const Home = () => {
         <div>
             <h1>My code is so trash this doesn't print</h1>
             <h2>To Do:</h2>
+
+            <p>Download repo on PC</p>
+
             <p>Re-enter password feature for signup</p>
             <p>Password hiding</p>
-            <p>Connect to github and merge changes</p>
-            <p>Download repo on PC</p>
             <p>Improve login and signup pages to be nicer</p>
-            <p>Monday: Ask Mark how the homepage should look and what he wants</p>
 
+            <p>Monday: Ask Mark how the homepage should look and what he wants</p>
         </div>
     );
 };
