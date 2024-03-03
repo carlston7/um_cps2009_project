e80ccb2ba712fdf5f3a2facca6a35286284cb573
Your Name <darren.garrett.20@um.edu.mt>
2024-03-03 15:10:22 +0100

    Changed how the server starts/connection to db is made

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const app = express();
 
 const cors = require("cors");
 
-require("dotenv").config({ path: "./config.env" });
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-//added the next two lines to test the landing page
+// Added the next two lines to test the landing page
 const fs = require('fs');
 const path = require('path');
 
@@ -17,8 +17,8 @@ app.use(express.json());
 
 app.use(require("./routes/record"));
 
-// Get MongoDB driver connection
-const dbo = require("./db/conn");
+//Get MongoDB driver connection
+const connect_db = require("./db/conn");
 
 // Route handler for the root URL (TEST)
 app.get('/', (req, res) => {
@@ -35,11 +35,21 @@ app.get('/', (req, res) => {
   });
 });  
 
-app.listen(port, () => {
-  // Perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+
+const start = async() => {
+  try{
+    // Makes a database connection
+    await connect_db();
+
+    // Starts the server
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  }catch(e){
+    console.error(e.message);
+  }
+}
+
+// Calling the function which makes a database
+// connection and starts the server
+start();
