fee7f7b0edcff3836492dd9e4ae0416725981aee
Carlston7 <carlston.azzopardi.18@um.edu.mt>
2024-03-04 19:39:12 +0100

    Added default parameters(credit and type)

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -3,6 +3,8 @@ const User = require('../models/users')
 exports.create_user = async (user_data) => {
     try{
         const user = new User(user_data);
+        user.credit = 0;
+        user.type = 'member';
         await user.save();
         return user;
     }catch (e){
