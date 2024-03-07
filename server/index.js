ba97962b2802cdc602b5f0d476ecd2a6caaeed5f
Your Name <darren.garrett.20@um.edu.mt>
2024-03-07 14:12:23 +0100

    Testing registration validation

--- server/index.js
+++ server/index.js
@@ -44,11 +44,19 @@ const body_parser = require('body-parser')
 app.use(body_parser.json());
 
 app.post('/signup', async (req, res) => {
+  console.log('testing console.log');
+
   try{
-    const user_data = req.body;
-    const user = await create_user(user_data);
-    res.status (201).json(user);
-    console.log(user_data);
+
+    const user_exists = await User.exists({ email_address: 'test@test.test' });
+
+    if (user_exists)
+    {
+      return res.status(400).json({ error: 'Email address already exists' });
+    }else{
+      const user = await create_user(req.body);
+      res.status (201).json(user);
+    }    
   }catch(e){
     console.error(e);
     res.status(500).send('Server Error');
