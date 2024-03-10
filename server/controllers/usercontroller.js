37bce6345dc8ca67329625755a3132736a53caf6
Your Name <darren.garrett.20@um.edu.mt>
2024-03-10 21:31:38 +0100

    Testing password encryption during sign up.

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,7 +1,12 @@
 const User = require('../models/users')
+const bcrypt = require('bcryptjs')
 
 exports.create_user = async (user_data) => {
     try{
+        const salt = await bcrypt.genSalt(10);
+        const hashed_pwd = await bcrypt.hash(user_data.password, salt);
+        user_data.password = hashed_pwd;
+
         const mappedData = {
             ...user_data,
             email_address: user_data.email,
