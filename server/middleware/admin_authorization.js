6bd6b82c356af94b5cdf0603a6ca3983c8954ad3
Your Name <darren.garrett.20@um.edu.mt>
2024-03-14 18:01:31 +0100

    ID-60, 21 (Admin authorization middleware and creation of courts)

--- server/middleware/admin_authorization.js
+++ server/middleware/admin_authorization.js
@@ -1,15 +1,12 @@
 // Authorization middleware
-const requireAdmin = (req, res, next) => {
-    const headers = req.headers;
-    //test
-    console.log(req.header['User-Type']);
+const requireAdmin = (req, res, next) => {    
     // Check if user is authenticated
     // if (!req.user) {
     //     return res.status(401).json({ message: "Unauthorized" });
     // }
     
     // Check if user is an admin
-    if (headers['User-Type'] !== "admin") {
+    if (req.headers['User-Type'] !=='admin') {
         const error = new Error("Forbidden");
         error.statusCode = 403;
         next(error);
