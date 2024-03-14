b5e7a77f9b6d378c02538f7fe6143c32e66c49ae
Your Name <darren.garrett.20@um.edu.mt>
2024-03-14 17:24:48 +0100

    ID-60, 21 (Admin authorization middleware and creation of courts)

--- server/index.js
+++ server/index.js
@@ -144,6 +144,7 @@ app.post('/payment', async (req, res) => {
 });
 
 const { requireAdmin } = require('./middleware/admin_authorization.js'); 
+const { create_court } = require('./controllers/courtcontroller.js');
 
 //app.post('/court', requireAdmin, async (req, res) => {
 app.post('/court', async (req, res) => { 
