6b764697d024ec23c2808aad989a238727bc0e54
JacobEllul <jacob.ellul.22@um.edu.mt>
2024-03-18 13:22:55 +0100

    fixing deployment and payments, ID: 26 & 40

--- frontend/src/components/PaymentForm.tsx
+++ frontend/src/components/PaymentForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 import axios from 'axios'; // Assuming you're using axios for HTTP requests
 
 const StripePaymentForm = () => {
