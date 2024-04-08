785102390aefe1c7ecb5edf6d04bc1686e5fcd8e
JacobEllul <jacob.ellul.22@um.edu.mt>
2024-04-08 15:31:30 +0200

    Added edit profile functionality for frontend (feature)

---
+++ frontend/src/pages/EditProfilePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import EditProfileComponent from '../components/EditProfileComponent';
+
+const EditProfilePage = () => {
+    return (
+        <div>
+            <EditProfileComponent />
+        </div>
+    );
+};
+
+export default EditProfilePage;
