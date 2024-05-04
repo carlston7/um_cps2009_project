1db5d9f87ffd880276d02d182330938087a7efb1
Jacob <jacob.ellul.22@um.edu.mt>
2024-05-04 23:45:03 +0200

    ID-7424455, fixed issue in splitting bill and added testing

--- server/tests/controller/userController.test.js
+++ server/tests/controller/userController.test.js
@@ -7,12 +7,12 @@ jest.mock('../../models/courts', () => ({
 
 describe("get_court_price function", () => {
     it("should return night price after 6 PM", async () => {
-        const price = await get_court_price("CourtA", "2024-05-06T20:00:00Z");
+        const price = await get_court_price("CourtA", new Date("2024-05-06T20:00:00Z").getHours());
         expect(price).toBe(60);
     });
 
     it("should return day price before 6 PM", async () => {
-        const price = await get_court_price("CourtA", "2024-05-06T10:00:00Z");
+        const price = await get_court_price("CourtA",  new Date("2024-05-06T10:00:00Z").getHours());
         expect(price).toBe(30);
     });
 });
